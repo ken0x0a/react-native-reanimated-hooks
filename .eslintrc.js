45853592369ce57c08200e4a138f8aeac9aeb7ba
Ken <ken0x0a+github@gmail.com>
2023-06-23 13:05:09 +0900

    chore: enable eslint rule curly "all"

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,10 @@ module.exports = {
   extends: [
     "@ken0x0a/eslint-config/base",
     "@ken0x0a/eslint-config/import",
-    "@ken0x0a/eslint-config/react",
-    "@ken0x0a/eslint-config/react-native",
     "@ken0x0a/eslint-config/typescript",
   ],
   rules: {
     "import/extensions": 0,
+    curly: [2, "all"],
   },
 };
